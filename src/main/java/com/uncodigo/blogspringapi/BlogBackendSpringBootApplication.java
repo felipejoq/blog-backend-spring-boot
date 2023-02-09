e54b539db154db914293df794c188799594097cb
felipejoq <felipejofreq@gmail.com>
2023-02-09 18:53:24 -0300

    Set up my project for make a blog RESTful api

--- src/main/java/com/uncodigo/blogspringapi/BlogBackendSpringBootApplication.java
+++ src/main/java/com/uncodigo/blogspringapi/BlogBackendSpringBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogBackendSpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlogBackendSpringBootApplication.class, args);
     }
-
 }
