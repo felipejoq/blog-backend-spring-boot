e4abe5f58629c687c43cc7a97212993c0c4fdae8
felipejoq <felipejofreq@gmail.com>
2023-02-09 19:46:22 -0300

    Configure mariadb connection and create my first entity class

---
+++ src/main/java/com/uncodigo/blogspringapi/entity/Post.java
@@ -0,0 +1,27 @@
+package com.uncodigo.blogspringapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false, columnDefinition = "text")
+    private String content;
+}
